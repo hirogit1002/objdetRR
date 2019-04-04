d44bba8217faa40a35ca257cdde64160af42049d
hirogit1002 <hiromokada@gmail.com>
2019-04-04 21:43:21 +0200

    program

--- run.py
+++ run.py
@@ -69,6 +69,5 @@ else :
     if(args.load=="c"):
         tfnet.load_from_ckpt()
     imgcv = cv2.imread(args.imgdir)
-    print(imgcv)
     result = tfnet.return_predict(imgcv)
-    print(results)
+    print(result)
